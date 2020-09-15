a8b7a1c1d30e738ed6625df74fdfc06d6af437f6
Kristinemalveda1985 <kristinemalveda@yahoo.com>
2020-09-15 11:01:02 +0200

    push docs file

---
+++ docs/precache-manifest.4a28ad88e5b11104bcca00738acf6d81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af9091dd30e335d6e882aa07933c165e",
+    "url": "/grit-react/index.html"
+  },
+  {
+    "revision": "66bfdb9c61104a7e9b6c",
+    "url": "/grit-react/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "2c4d598930103523819f",
+    "url": "/grit-react/static/js/2.049355c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/grit-react/static/js/2.049355c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66bfdb9c61104a7e9b6c",
+    "url": "/grit-react/static/js/main.dde84d2f.chunk.js"
+  },
+  {
+    "revision": "bb4d3d5a83aa3b671523",
+    "url": "/grit-react/static/js/runtime-main.059892ae.js"
+  }
+]);
